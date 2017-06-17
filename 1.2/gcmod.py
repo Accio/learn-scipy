96791511101a64913619ca3448301a118d87479a
Jitao David Zhang <davidvonpku@gmail.com>
2017-06-17 21:58:22 +0200

    add main to be run directly from command line or %run

--- 1.2/gcmod.py
+++ 1.2/gcmod.py
@@ -1,4 +1,4 @@
-"David's first module"
+"A module for GC content"
 
 GCs = ('g','c')
 
@@ -12,4 +12,8 @@ def gc_count(seq):
 def gc_perc(seq):
 	return(gc_count(seq)/len(seq))
 
-
+if __name__ == '__main__':
+	demoseq = 'ATGCTGCA'
+	print("Demo seq: " + demoseq)
+	print("GC count: " + str(gc_count(demoseq)))
+	print("GC perc: " + str(gc_perc(demoseq)))
