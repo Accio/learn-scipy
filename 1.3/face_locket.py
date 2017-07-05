979cb61e096ef11315737d16bf2f3bb715adeeb1
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-07-05 08:41:35 +0200

    (1) circle changed to an ellipsoid (2) plt.imshow is used instead of plt.show (3) matplotlib.pyplot.ion() is set to allow an interactive session

--- 1.3/face_locket.py
+++ 1.3/face_locket.py
@@ -1,19 +1,23 @@
 #/usr/bin/python3
 
+'''When running in ipython, use 'ipython --pylab' for an interactive shell that is matplotlib aware, and/or set matplotlib.pyplot.ion()'''
+
 import numpy as np
 from scipy import misc
 import pylab as plt
+import matplotlib
+
+matplotlib.pyplot.ion()
 
 face = misc.face(gray=True)
-plt.show(face)
 
 sy, sx = face.shape
 y,x = np.ogrid[0:sy, 0:sx]
 y.shape, x.shape
 
 centerx, centery = (660, 300)
-mask = ((y - centery)**2 + (x - centerx)**2) > 230**2
+mask = ((y - centery)**2/200**2 + (x - centerx)**2/300**2) > 1 
 
 face[mask]=0
-plt.show(face)
+plt.imshow(face)
 
